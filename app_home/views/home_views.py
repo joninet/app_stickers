fde54a8b4c9269c9641d672d4acba39cbc0b0a9b
Jonathan Desplats <jon.desplats@gmail.com>
2025-12-07 23:04:01 -0300

    create add cart

--- app_home/views/home_views.py
+++ app_home/views/home_views.py
@@ -6,21 +6,6 @@ import random
 def home(request):
     products = Product.objects.filter(is_available=True)
 
-    gradients = [
-        "from-red-400 to-pink-500",
-        "from-blue-400 to-purple-500",
-        "from-green-400 to-emerald-500",
-        "from-yellow-400 to-orange-500",
-        "from-indigo-400 to-blue-500",
-        "from-purple-400 to-fuchsia-500",
-        "from-pink-400 to-rose-500",
-        "from-red-400 to-yellow-500",
-    ]
-
-    # Agrego una clase random a cada producto
-    for p in products:
-        p.color_class = random.choice(gradients)
-
     context = {
         'products': products
     }
